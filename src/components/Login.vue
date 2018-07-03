78d9e7b4294557a52f9fa1e75a04ad51f88c19e4
fsabria <fsabria@NINFALI.sadm.bx1>
2018-07-03 15:05:05 +0200

    page sign in doneroad done: faire lien avec firebase

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email"></input>
+    <input type="password" placeholder="Password"></input>
+    <button>Connection</button>
+    <p>You don't hanve an account? You can create one</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+
+<style scoped>
+
+</style>
