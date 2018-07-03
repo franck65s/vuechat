934559fe26ce3a234baa76b3b3388c65ee724d8a
fsabria <fsabria@NINFALI.sadm.bx1>
2018-07-03 17:00:05 +0200

    Next step creation sign up view

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,21 +4,39 @@
     <input type="text" placeholder="Email"></input>
     <input type="password" placeholder="Password"></input>
     <button>Connection</button>
-    <p>You don't hanve an account? You can create one</p>
+    <p>You don't have an account? You can create one</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'login',
-  data: function () {
-    return {}
+  data () {
   },
   methods: {}
 }
 </script>
-
-
-<style scoped>
-
+<style>
+  .login{
+    color:red;
+    margin-top: 40px;
+  }
+  input{
+    margin: 10px 0;
+    width: 20%;
+    padding: 15px;
+  }
+  button{
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+  }
+  p {
+    margin-top: 40px;
+    font-size: 13px;
+  }
+  p a {
+    test-decoration: underline;
+    cursor: pointer;
+  }
 </style>
